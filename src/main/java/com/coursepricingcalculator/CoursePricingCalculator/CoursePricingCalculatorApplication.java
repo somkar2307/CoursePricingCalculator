7f71fb465f5be05605a2a9801a572e63fbbee0f9
somkar2307 <103460959+somkar2307@users.noreply.github.com>
2023-02-09 04:53:37 -0500

    Update CoursePricingCalculatorApplication.java

--- src/main/java/com/coursepricingcalculator/CoursePricingCalculator/CoursePricingCalculatorApplication.java
+++ src/main/java/com/coursepricingcalculator/CoursePricingCalculator/CoursePricingCalculatorApplication.java
@@ -8,6 +8,7 @@ public class CoursePricingCalculatorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoursePricingCalculatorApplication.class, args);
+		//System.out.println("Hello World");
 	}
 
 }
