f0d3f33d702ba8120fecb9c8d91df8f7af9c5288
somkar2307 <103460959+somkar2307@users.noreply.github.com>
2023-02-09 04:52:34 -0500

    first commit
    created project using spring initializr

---
+++ src/main/java/com/coursepricingcalculator/CoursePricingCalculator/CoursePricingCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.coursepricingcalculator.CoursePricingCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursePricingCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursePricingCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coursepricingcalculator/CoursePricingCalculator/CoursePricingCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coursepricingcalculator.CoursePricingCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursePricingCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
